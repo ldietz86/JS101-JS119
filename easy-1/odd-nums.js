48d1ef01bb4136348fea74f686cfea5a20e9ec17
Lakpa Dietz <ldietz086@gmail.com>
2025-01-03 16:01:12 -0800

    Exercise 1 and 2

---
+++ easy-1/odd-nums.js
@@ -0,0 +1,19 @@
+/**
+ * Log all odd numbers from 1 to 99, inclusive, to the console,
+ * with each number on a separate line.
+ */
+
+let num = 1;
+while (num < 100) {
+  console.log(num);
+  num += 2;
+}
+
+for (let i = 1; i < 100; i += 2) {
+  console.log(i);
+}
+
+// LS solution:
+for (let number = 1; number < 100; number += 2) {
+  console.log(number);
+}
