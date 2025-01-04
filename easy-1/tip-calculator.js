4f645a220f5ea52fbc24aa1b09fcc8c1ee1e2ff4
Lakpa Dietz <ldietz086@gmail.com>
2025-01-04 15:14:51 -0800

    Exercise 5

---
+++ easy-1/tip-calculator.js
@@ -0,0 +1,14 @@
+/**
+ * Create a simple tip calculator. The program should prompt for a bill amount and a tip rate.
+ * The program must compute the tip, and then log both the tip and the total amount of the bill to the console.
+ * You can ignore input validation and assume that the user will enter numbers.
+ */
+let readlineSync = require("readline-sync");
+
+let billAmount = parseFloat(readlineSync.prompt("What is the bill?"));
+let tipPercentage = parseInt(realineSync.prompt("What is the tip percentage?"));
+
+let tip = billAmount * (tipPercentage / 100);
+
+console.log(`The tip is ${tip.toFixed(2)}`);
+console.log(`The total is ${(billAmount + tip).toFixed(2)}`);
