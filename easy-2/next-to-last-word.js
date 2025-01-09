cb9912588b7116a519a626eb5d04f1875b204f15
Lakpa Dietz <ldietz086@gmail.com>
2025-01-09 09:49:22 -0800

    Added further exploration

--- easy-2/next-to-last-word.js
+++ easy-2/next-to-last-word.js
@@ -6,9 +6,33 @@
 
 function penultimate(str) {
   let words = str.split(" ");
-  let secondToLastWord = words.slice(-2)[0]
-  return str = secondToLastWord;
+  let secondToLastWord = words.slice(-2)[0];
+  return secondToLastWord;
 }
 
 console.log(penultimate("last word") === "last"); // true
 console.log(penultimate("Launch School is great!") === "is"); // true
+
+/**
+ * Further exploration
+ * Edge cases: empty string, one word, even number of words, extra spaces.
+ */
+
+function middleWord(str) {
+  let words = str.split(" ").filter((word) => word !== "");
+
+  if (words.length === 0) {
+    return "Empty string";
+  } else if (words.length === 1) {
+    return words[0];
+  }
+
+  let middleIndex = Math.floor(words.length / 2);
+  return words[middleIndex];
+}
+
+//LS solution
+function penultimate(words) {
+  let wordsArray = words.split(" ");
+  return wordsArray[wordsArray.length - 2];
+}
