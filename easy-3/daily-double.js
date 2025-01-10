f92c84947d749bec14ff3a90e2537a5ec850f647
Lakpa Dietz <ldietz086@gmail.com>
2025-01-09 16:45:23 -0800

    Changed file name and added directory

---
+++ easy-3/daily-double.js
@@ -0,0 +1,5 @@
+/**
+ * Write a function that takes a string argument and returns a new string that contains the value of the 
+ * original string with all consecutive duplicate characters collapsed into a single character.
+ */
+
