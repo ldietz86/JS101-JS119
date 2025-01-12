27120b851123ead5d5d3897d39616246ac68b6b6
Lakpa Dietz <ldietz086@gmail.com>
2025-01-11 19:10:54 -0800

    Exercise 2 Easy-3

--- easy-3/bannerizer.js
+++ easy-3/bannerizer.js
@@ -1,1 +1,28 @@
-// Write a function that will take a short line of text, and write it to the console log within a box. You may assume that the output will always fit in your browser window.
+// * Write a function that will take a short line of text, and write it to the console log within a box. You may assume that the output will always fit in your browser window.
+
+function logInBox(str) {
+  let boxLines = [];
+  boxLines.push(`+${"-".repeat(str.length + 2)}+`);
+  boxLines.push(`|${" ".repeat(str.length + 2)}|`);
+  boxLines.push(`| ${str} |`);
+  boxLines.push(`|${" ".repeat(str.length + 2)}|`);
+  boxLines.push(`+${"-".repeat(str.length + 2)}+`);
+
+  console.log(boxLines.join("\n"));
+}
+
+//LS solution
+function logInBox(str) {
+  let strLength = str.length;
+
+  let border = `+${"-".repeat(str.length + 2)}+`;
+  let emptyLine = `|${" ".repeat(str.length + 2)}|`;
+
+  console.log(border);
+  console.log(emptyLine);
+  console.log(`| ${str} |`);
+  console.log(emptyLine);
+  console.log(border);
+}
+
+logInBox("To boldly go where no one has gone before.");
