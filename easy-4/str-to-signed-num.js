82b6982fdfc77090f3cd34ec01e36419cc904305
Lakpa Dietz <ldietz086@gmail.com>
2025-01-24 13:29:27 -0800

    Exercise 10-4

--- easy-4/str-to-signed-num.js
+++ easy-4/str-to-signed-num.js
@@ -6,11 +6,49 @@
  * You may assume the string will always contain a valid number. You may not use parseInt() and Number(). You may, however, use the stringToInteger() function from the previous lesson.
  */
 
+function stringToInteger(string) {
+  const DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+  };
+  let arrayOfDigits = string.split("").map((char) => DIGITS[char]);
+  let value = 0;
+  arrayOfDigits.forEach((digit) => (value = 10 * value + digit));
+  return value;
+}
+
 function stringToSignedInteger(str) {
- 
+  let sign = 1;
+  if (str[0] === "-") {
+    sign = -1;
+    str = str.slice(1);
+  } else if (str[0] === "+") {
+    str = str.slice(1);
+  }
+
+  return sign * stringToInteger(str);
 }
 
 console.log(stringToSignedInteger("4321") === 4321); // logs true
 console.log(stringToSignedInteger("-570") === -570); // logs true
 console.log(stringToSignedInteger("+100") === 100); // logs true
 
+//LS solution
+function stringToSignedInteger(string) {
+  switch (string[0]) {
+    case "-":
+      return -stringToInteger(string.slice(1, string.length));
+    case "+":
+      return stringToInteger(string.slice(1, string.length));
+    default:
+      return stringToInteger(string);
+  }
+}
