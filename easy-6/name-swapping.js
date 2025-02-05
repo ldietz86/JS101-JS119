dd4afa07b98d36824ac121a136e34d4fe2fafe95
Lakpa Dietz <ldietz086@gmail.com>
2025-02-04 16:54:14 -0800

    Exercise 6-6

--- easy-6/name-swapping.js
+++ easy-6/name-swapping.js
@@ -1,11 +1,20 @@
 /**
- * Write a function that takes a string argument consisting of a first name, a space, and a last name, 
+ * Write a function that takes a string argument consisting of a first name, a space, and a last name,
  * and returns a new string consisting of the last name, a comma, a space, and the first name.
  */
 
 function swapName(str) {
-    return str.split(' ').reverse().join(", ");
+  return str.split(" ").reverse().join(", ");
 }
 
-swapName('Joe Roberts');    // "Roberts, Joe"
+swapName("Joe Roberts"); // "Roberts, Joe"
 
+//Further exploration
+function swapName(str) {
+  let words = str.split(" ");
+  let lastName = words.pop();
+
+  return `${lastName}, ${words.join(" ")}`;
+}
+
+swapName("Karl Oskar Henriksson Ragvals"); // "Ragvals, Karl Oskar Henriksson"
