1f1526a8124a4fef6223efa3ed0d50eb72d56e93
Lakpa Dietz <ldietz086@gmail.com>
2025-02-06 14:54:46 -0800

    List processing q2

---
+++ list-processing/alphabetical-nums.js
@@ -0,0 +1,88 @@
+/**
+ * Write a function that takes an array of integers between 0 and 19 and returns an array of those integers sorted based on the English word for each number.
+ *
+ * Input: Array of numbers
+ * Output: Array of numbers sorted alphabetically based on their English name
+ *
+ *
+ */
+
+function alphabeticNumberSort(arr) {
+  let numberWords = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
+
+  let wordsArray = arr.map((num) => {
+    return numberWords[num];
+  });
+
+  let sortedWords = wordsArray.sort();
+
+  let sortedNumbers = sortedWords.map((word) => {
+    return numberWords.indexOf(word);
+  });
+
+  return sortedNumbers;
+}
+
+alphabeticNumberSort([
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+]);
+// [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17, 6, 16, 10, 13, 3, 12, 2, 0]
+
+//LS solution
+const NUMBER_WORDS = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
+
+function wordSort(num1, num2) {
+  if (NUMBER_WORDS[num1] > NUMBER_WORDS[num2]) {
+    return 1;
+  } else if (NUMBER_WORDS[num1] < NUMBER_WORDS[num2]) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function alphabeticNumberSort(array) {
+  return [...array].sort(wordSort);
+}
