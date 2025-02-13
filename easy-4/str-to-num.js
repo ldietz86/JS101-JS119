0eb9993bf0e05fe669e96adee6349b12dbbed411
Lakpa Dietz <ldietz086@gmail.com>
2025-02-13 10:53:50 -0800

    Added solution using reduce

--- easy-4/str-to-num.js
+++ easy-4/str-to-num.js
@@ -17,7 +17,6 @@ function stringToInteger(str) {
   return str * 1; //or str / 1, str - 1 + 1, str - 0 (Any Math operation will convert a string to a number bc JS will perform type coercion)
 }
 
-
 //LS solution
 function stringToInteger(string) {
   const DIGITS = {
@@ -35,5 +34,24 @@ function stringToInteger(string) {
   let arrayOfDigits = string.split("").map((char) => DIGITS[char]);
   let value = 0;
   arrayOfDigits.forEach((digit) => (value = 10 * value + digit));
+  return value;
+}
+
+//Solution using reduce to accumulate final value
+function stringToInteger(string) {
+  const DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+  };
+  let arrayOfDigits = string.split("").map((char) => DIGITS[char]);
+  let value = arrayOfDigits.reduce((acc, digit) => acc * 10 + digit, 0);
   return value;
 }
